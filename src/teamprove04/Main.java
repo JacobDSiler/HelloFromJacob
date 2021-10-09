dc10a8eaf9a170d7338ef5494f5d2e59bf100e32
benellis1919 <ell15027@byui.edu>
2021-10-09 13:11:15 -0600

    Forgot a semi-colon haha

--- src/teamprove04/Main.java
+++ src/teamprove04/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Jacob.");
-        System.out.println("Hey Jacob, it's good to meet you! My name is Ben.")
+        System.out.println("Hey Jacob, it's good to meet you! My name is Ben.");
     }
 }
