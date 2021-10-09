e9d69beb83c53c1e2978c0d0e83fd938254e645b
benellis1919 <ell15027@byui.edu>
2021-10-09 13:01:46 -0600

    Hello Jacob

--- src/teamprove04/Main.java
+++ src/teamprove04/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Jacob.");
+        System.out.println("Hey Jacob, it's good to meet you! My name is Ben.")
     }
 }
